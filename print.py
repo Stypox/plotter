b9d017e43d0a38ceb82bcc5c246b35a88aec5239
Stypox <stypox@pm.me>
2019-12-29 14:54:36 +0100

    Add binary subcommand and connection options
    Sending is still not implemented

--- print.py
+++ print.py
@@ -3,18 +3,15 @@ from enum import Enum
 import text_to_gcode.text_to_gcode as text_to_gcode
 import gcode_parser.gcode_parser as gcode_parser
 
-class Subcommand(Enum):
-	gcode = 0,
-	text  = 1
-
 def parseArgs(namespace):
 	argParser = argparse.ArgumentParser(fromfile_prefix_chars="@",
 		description="Print something with the connected plotter")
-	subparsers = argParser.add_subparsers(description="Format subcommands")
+	subparsers = argParser.add_subparsers(dest="subcommand",
+		description="Format subcommands")
 
 	ioGroup = argParser.add_argument_group("Output options")	
 	ioGroup.add_argument("-o", "--output", type=argparse.FileType('w'), required=False, metavar="FILE",
-		help="File in which to save the generated gcode")
+		help="File in which to save the generated gcode (will be ignored if using binary subcommand)")
 	ioGroup.add_argument("-b", "--binary-output", type=argparse.FileType('wb'), required=False, metavar="FILE",
 		help="File in which to save the binary data ready to be fed to the plotter")
 	ioGroup.add_argument("-l", "--log", type=argparse.FileType('w'), required=False, metavar="FILE",
@@ -27,6 +24,20 @@ def parseArgs(namespace):
 		help="The size of the print area in millimeters (e.g. 192.7x210.3)")
 	genGroup.add_argument("-d", "--dilation", type=float, default=1.0, metavar="FACTOR",
 		help="Dilation factor to apply (useful to convert mm to steps)")
+
+	connGroup = argParser.add_argument_group("Plotter connectivity options")
+	connGroup.add_argument("--simulate", action="store_true",
+		help="Simulate sending data to the plotter without really opening a connection. Useful with logging enabled to debug the commands sent.")
+	connGroup.add_argument("--port", "--serial-port", type=str, metavar="PORT", dest="serial_port",
+		help="The serial port the plotter is connected to (required unless there is --simulate)")
+	connGroup.add_argument("--baud", "--baud-rate", type=int, metavar="RATE", dest="baud_rate",
+		help="The baud rate to use for the connection with the plotter. It has to be equal to the plotter baud rate. (required unless there is --simulate)")
+
+
+	binParser = subparsers.add_parser("binary", help="Send binary files directly to the plotter")
+	bpDataGroup = binParser.add_argument_group("Data options")
+	bpDataGroup.add_argument("-i", "--input", type=argparse.FileType('rb'), required=True, metavar="FILE",
+		help="Binary file from which to read the raw data to send to the plotter")
 
 
 	gcodeParser = subparsers.add_parser("gcode", help="Print gcode with the plotter")
@@ -67,19 +78,18 @@ def parseArgs(namespace):
 	except:
 		argParser.error(f"invalid formatting for --size: {namespace.size}")
 
-	# check subcommands (required=True does not work)
-	if hasattr(namespace, "use_g"):
-		Args.subcommand = Subcommand.gcode
-		if namespace.use_g == False and namespace.feed_visible_below is None and namespace.speed_visible_below is None:
-			argParser.error(f"at least one of --use-g, --feed-visible-below, --speed-visible-below should be provided to subcommand gcode")
+	if not namespace.simulate:
+		if namespace.serial_port is None:
+			argParser.error(f"--serial-port is required unless there is --simulate")
+		if namespace.baud_rate is None:
+			argParser.error(f"--baud-rate is required unless there is --simulate")
 
-	elif hasattr(namespace, "gcode_directory"):
-		Args.subcommand = Subcommand.text
-		if hasattr(namespace, "use_g"):
-			argParser.error(f"only one subcommand from the following can be used: text, gcode")
+	# check that a subcommand was selected (required=True is buggy)
+	if namespace.subcommand is None:
+		argParser.error(f"exactly one subcommand from the following is required: binary, gcode, text")
 
-	else:
-		argParser.error(f"exactly one subcommand from the following is required: text, gcode")
+	if namespace.subcommand == "gcode" and namespace.use_g == False and namespace.feed_visible_below is None and namespace.speed_visible_below is None:
+		argParser.error(f"at least one of --use-g, --feed-visible-below, --speed-visible-below should be provided to subcommand gcode")
 
 class Args:
 	pass
@@ -106,29 +116,33 @@ def parseGcode(gcodeData):
 
 	return parsedGcode
 
-def writeOutputs(data, binaryData):
-	if Args.output is not None:
-		Args.output.write(data)
-	if Args.binary_output is not None:
-		Args.binary_output.write(binaryData)
-
 def main():
 	parseArgs(Args)
 	
-	gcodeData = ""
-	if Args.subcommand == Subcommand.gcode:
-		gcodeData = Args.input.read()
-	elif Args.subcommand == Subcommand.text:
-		gcodeData = textToGcode(Args.input.read())
+	binaryData = b""
+	if Args.subcommand == "binary":
+		binaryData = Args.input.read()
+	else:
+		gcodeData = ""
+		if Args.subcommand == "gcode":
+			gcodeData = Args.input.read()
+		elif Args.subcommand == "text":
+			gcodeData = textToGcode(Args.input.read())
 
-		# settings for gcode parser
-		Args.use_g = True
-		Args.feed_visible_below = None
-		Args.speed_visible_below = None
-	
-	parsedGcode = parseGcode(gcodeData)
-	binaryData = gcode_parser.toBinaryData(parsedGcode)
-	writeOutputs(gcode_parser.toGcode(parsedGcode), binaryData)
+			# settings for gcode parser
+			Args.use_g = True
+			Args.feed_visible_below = None
+			Args.speed_visible_below = None
+		else:
+			raise AssertionError()
+		
+		parsedGcode = parseGcode(gcodeData)
+		binaryData = gcode_parser.toBinaryData(parsedGcode)
+		if Args.output is not None:
+			Args.output.write(gcode_parser.toGcode(parsedGcode))
+
+	if Args.binary_output is not None:
+		Args.binary_output.write(binaryData)
 
 if __name__ == "__main__":
 	main()
